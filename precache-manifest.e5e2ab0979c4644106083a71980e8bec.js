3bc241b073037b5d861573f34fbaf3d93807dfc2
Pavle Popovic <pavlepopovic90@gmail.com>
2020-08-26 10:03:25 +0200

    Updates

---
+++ precache-manifest.e5e2ab0979c4644106083a71980e8bec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4cba945fc81e8c6d0186417155df70",
+    "url": "/hsdbpavlepopovic/index.html"
+  },
+  {
+    "revision": "e484b9c5db0dff8cc2cb",
+    "url": "/hsdbpavlepopovic/static/css/main.bd271b57.chunk.css"
+  },
+  {
+    "revision": "53e7b4f06d6c6aec3ff9",
+    "url": "/hsdbpavlepopovic/static/js/2.13bf40b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hsdbpavlepopovic/static/js/2.13bf40b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e484b9c5db0dff8cc2cb",
+    "url": "/hsdbpavlepopovic/static/js/main.7a52c75f.chunk.js"
+  },
+  {
+    "revision": "ef06c7326950c7e802db",
+    "url": "/hsdbpavlepopovic/static/js/runtime-main.a1d206fe.js"
+  },
+  {
+    "revision": "8ad82b1dc550319993a7d6c932b2656d",
+    "url": "/hsdbpavlepopovic/static/media/RobotoMono-Medium.8ad82b1d.ttf"
+  },
+  {
+    "revision": "e5ca8c0ac474df46fe45840707a0c483",
+    "url": "/hsdbpavlepopovic/static/media/RobotoMono-Regular.e5ca8c0a.ttf"
+  }
+]);
